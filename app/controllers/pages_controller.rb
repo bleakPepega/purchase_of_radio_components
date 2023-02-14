6798ad21726c94c2e508a0a4770f8d2c89e62ac5
bleak_Pepega <matraskin879@gmail.com>
2023-02-15 02:28:47 +0300

    add all

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,12 @@
 class PagesController < ApplicationController
+  require 'nokogiri'
+
   def index
+    require 'open-uri'
+    url = 'http://www.cubecinema.com/programme'
+    html = URI.open(url)
+
     @fields = %w[test pepega from telega]
+
   end
 end
